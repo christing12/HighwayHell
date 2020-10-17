81e2f2e3c7be0559c2cae188ba312fbf328e9b23
kovesdy <kovesdy@usc.edu>
2020-10-17 12:30:41 -0700

    Continue to smooth car controller

--- Assets/Scripts/PlayerController/PlayerCar.cs
+++ Assets/Scripts/PlayerController/PlayerCar.cs
@@ -10,10 +10,10 @@ namespace UnityTemplateProjects.PlayerController
         public Rigidbody Rigidbody;
         public Vector2 Input;
         public IInput[] Inputs;
-        public float MAX_FORWARD_SPEED = 10.0f;
-        public float MIN_FORWARD_SPEED = 5.0f;
-        public float START_SPEED = 7.5f;
-        public float ACCEL = 1.0f;
+        public float MAX_FORWARD_SPEED = 0.1f;
+        public float MIN_FORWARD_SPEED = 0.075f;
+        public float START_SPEED = 0.1f;
+        public float ACCEL = 0.2f;
         public float TURN = 2.0f;
         public float MAX_TURN = 10.0f;
         public float ANG_DECEL = 1.0f;
@@ -71,7 +71,19 @@ namespace UnityTemplateProjects.PlayerController
             Vector3 fwd = turnAngle * Rigidbody.transform.forward;
 
             // Finding new linear velocity and updating
-            Vector3 adjVelocity = Rigidbody.velocity + new Vector3(0,0, linear * ACCEL * Time.deltaTime);
+            Vector3 forwardAccel = Rigidbody.transform.forward * linear * ACCEL;
+
+            Vector3 adjVelocity = Rigidbody.velocity + forwardAccel*Time.deltaTime;
+
+            // Clamping to forward and backward speed
+            if (adjVelocity.magnitude > MAX_FORWARD_SPEED)
+            {
+                adjVelocity = Rigidbody.transform.forward * MAX_FORWARD_SPEED;
+            }
+            else if (adjVelocity.magnitude < MIN_FORWARD_SPEED)
+            {
+                adjVelocity = Rigidbody.transform.forward * MIN_FORWARD_SPEED;
+            }
 
             // Clamping to min/max rotational speed
             if (Mathf.Abs(adjVelocity.x) > MAX_TURN)
@@ -83,17 +95,7 @@ namespace UnityTemplateProjects.PlayerController
                 else
                 {
                     adjVelocity.x = MAX_TURN;
-                } 
-            }
-
-            // Clamping to min/max linear speed
-            if (adjVelocity.z > MAX_FORWARD_SPEED)
-            {
-                adjVelocity.z = MAX_FORWARD_SPEED;
-            }
-            else if (adjVelocity.z < MIN_FORWARD_SPEED)
-            {
-                adjVelocity.z = MIN_FORWARD_SPEED;
+                }
             }
 
             Rigidbody.velocity = adjVelocity;
