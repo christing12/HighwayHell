4fe0a87c18b1995f1ceb5cd3601289212bc7a60e
kovesdy <kovesdy@usc.edu>
2020-10-17 14:34:28 -0700

    Landmines

---
+++ Assets/Scripts/Landmine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landmine : MonoBehaviour
+{
+    Rigidbody rb;
+    public float explosionPower = 500f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rb = collision.rigidbody; //gets Players rigid body
+            rb.AddForce(explosionPower/1.5f, explosionPower, 0);
+        }
+    }
+}
