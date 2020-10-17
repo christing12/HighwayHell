97b08e54fc77813760ed2e0a7b6b018fed8a5ea6
DominickTamras <31298279+DominickTamras@users.noreply.github.com>
2020-10-16 22:27:28 -0700

    Add files via upload

---
+++ Assets/Scripts/Sukk_Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sukk_Movement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb_movement;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb_movement = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb_movement.velocity = new Vector3(speed,0,0);
+    }
+}
+
