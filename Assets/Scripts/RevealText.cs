abdfe0b2779fbd3328e08c9decdc76500120b86f
David Zheng <davidzheng141@gmail.com>
2020-10-18 00:46:33 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/christing12/MonsterFreight""
    This reverts commit 31248bedd25635a1e6130c76e00ef11cdf573dd5.

---
+++ Assets/Scripts/RevealText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RevealText : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void Reveal()
+    {
+        text.gameObject.SetActive(true);
+    }
+}
