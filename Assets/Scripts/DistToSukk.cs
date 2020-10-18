90508d2c3a737672c941a7a9e3df4dc9c134e7b4
kovesdy <kovesdy@usc.edu>
2020-10-18 01:27:00 -0700

    Finished sukk distance system

--- Assets/Scripts/DistToSukk.cs
+++ Assets/Scripts/DistToSukk.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 //Calculates the distance between Sukk and the player
@@ -11,6 +12,8 @@ public class DistToSukk : MonoBehaviour
     public Rigidbody enemy; //Sukk monster
     public Rigidbody player; //Player car
     public TextMeshProUGUI distText;
+    public float deathThreshold; //distance before death is counted (behind the sukk)
+    public float waitTime; //Number of seconds before killing the player after Sukk has caught up
 
     private float dist; //Z distance between sukk and player car
 
@@ -18,8 +21,22 @@ public class DistToSukk : MonoBehaviour
     void Update()
     {
         //When dist is positive, the player is OK and when dist is negative the player has lost
-        dist = enemy.position.z - player.position.z;
+        dist = player.position.z - enemy.position.z;
         //Update distance to sukk text
         distText.SetText(dist.ToString("F0"));
+
+        //Check if Sukk has caught up, and if it has, start coroutine
+        if (dist <= deathThreshold)
+        {
+            StartCoroutine(waitForDeathCoRoutine());
+        }
+    }
+
+    IEnumerator waitForDeathCoRoutine()
+    {
+        yield return new WaitForSeconds(waitTime);// Wait for one second
+
+        //After the timer has completed, load death scene
+        SceneManager.LoadScene("DeathScene");
     }
 }
